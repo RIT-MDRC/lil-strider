f6bc8b7abc9c230d35ed8fe7d15f30bfdccc8882
Hiro <hiroto125takeuchi@gmail.com>
2025-10-17 18:29:34 -0400

    feat: angle param

--- src/arm.py
+++ src/arm.py
@@ -1,10 +1,24 @@
+import argparse
+
 from components import servo as servo_actions
 from statemanagement.device import configure_device
+
+# Set up argument parser
+parser = argparse.ArgumentParser(description="Control servo angles")
+parser.add_argument(
+    "-a",
+    "--angle",
+    type=int,
+    default=0,
+    help="Angle to set for all servos (default: 0)",
+)
+
+args = parser.parse_args()
 
 configure_device("config.json")
 
 SERVOS = list(map(lambda i: f"servo{i}", range(1, 18)))
-ANGLE = 0
+ANGLE = args.angle
 
 
 for servo in SERVOS:
